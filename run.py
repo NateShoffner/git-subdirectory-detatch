8d055433a51d6b8d9090710aae15a5975f22892d
Nate Shoffner <nate.shoffner@gmail.com>
2016-02-17 17:46:18 -0500

    pep8

--- run.py
+++ run.py
@@ -5,9 +5,21 @@ import sys
 
 if len(sys.argv) > 1:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--repo_dir', type=str, nargs='?', help='Path of original repo.')
-    parser.add_argument('--output_dir', type=str, nargs='?',help='Path of final output directory.')
-    parser.add_argument('--subdirectory_path', type=str, nargs='?',help='Relative path of subdirectory path to isolate.')
+    parser.add_argument(
+        '--repo_dir',
+        type=str,
+        nargs='?',
+        help='Path of original repo.')
+    parser.add_argument(
+        '--output_dir',
+        type=str,
+        nargs='?',
+        help='Path of final output directory.')
+    parser.add_argument(
+        '--subdirectory_path',
+        type=str,
+        nargs='?',
+        help='Relative path of subdirectory path to isolate.')
     args = parser.parse_args()
 
     original_repo_path = args.repo_dir
@@ -16,17 +28,23 @@ if len(sys.argv) > 1:
 else:
     original_repo_path = input('Original Repo Path: ')
     new_repo_path = input('New Repo Path: ')
-    subdirectory_path = input('Subdirectory path to isolate (relative to %s): ' % new_repo_path)
+    subdirectory_path = input(
+        'Subdirectory path to isolate (relative to %s): ' %
+        new_repo_path)
 
 if not os.path.exists(new_repo_path):
     os.makedirs(new_repo_path)
 else:
-	print 'OUTPUT DIRECTORY ALREADY EXISTS'
-	sys.exit(0)
+    print 'OUTPUT DIRECTORY ALREADY EXISTS'
+    sys.exit(0)
 
 # clone original repo to new working repo
 print 'Cloning original repo...'
-subprocess.call(['git', 'clone', '--no-hardlinks', original_repo_path, new_repo_path])
+subprocess.call(['git',
+                 'clone',
+                 '--no-hardlinks',
+                 original_repo_path,
+                 new_repo_path])
 
 os.chdir(new_repo_path)
 
@@ -36,7 +54,13 @@ if sys.platform == 'win32' and subdirectory_path.count('\\') > 1:
 
 # discard everything but our subdirectory, promote to root level
 print 'Discarding unwanted changes...'
-subprocess.call(['git', 'filter-branch', '--subdirectory-filter', subdirectory_path, 'HEAD', '--', '--all'])
+subprocess.call(['git',
+                 'filter-branch',
+                 '--subdirectory-filter',
+                 subdirectory_path,
+                 'HEAD',
+                 '--',
+                 '--all'])
 subprocess.call(['git', 'reset', '--hard'])
 
 
